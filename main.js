f7a7f952bb39baa711ef38a160f55fbeeabe738a
Achan <tapitapeh@gmail.com>
2018-09-15 01:35:03 +0800

    update

--- main.js
+++ main.js
@@ -1,30 +1,67 @@
+var toggle = true;
 function showTime(){
-  var date = new Date();
-  var h = date.getHours(); // 0 - 23
-  var m = date.getMinutes(); // 0 - 59
+  setInterval(function(){
+
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    
+    var time = h + ":" + m;
+    document.getElementById("hour").innerText = h;
+    document.getElementById("hour").textContent = h;
+    
+    document.getElementById("minute").innerText = m;
+    document.getElementById("minute").textContent = m;
+    
+    document.getElementById("colon").style.visibility = toggle?"visible":"hidden";
+    toggle=!toggle;
+  }, 1000);
   
-  h = (h < 10) ? "0" + h : h;
-  m = (m < 10) ? "0" + m : m;
-  
-  var time = h + ":" + m;
-  document.getElementById("clock").innerText = time;
-  document.getElementById("clock").textContent = time;
-  
-  setTimeout(showTime, 1000);
+  // setTimeout(showTime, 1000);
   
 }
 
 showTime();
 
-function getPrayerTimes(){
-  var times = {
-    'imsak': '05:39',
-    'subuh': '05:49',
-    'syuruk': '07:03',
-    'dhuha': '07:27',
-    'zohor': '13:12',
-    'asar': '16:12',
-    'maghrib': '19:15',
-    'isyak': '20:24'
+var request = new XMLHttpRequest();
+request.open('GET', 'http://api.aladhan.com/v1/timingsByCity?city=kajang&country=malaysia&method=11', true);
+
+request.onload = function() {
+  if (request.status >= 200 && request.status < 400) {
+    // Success!
+    var resp = request.responseText;
+
+    console.log(JSON.parse(request.responseText) );
+    renderTime( JSON.parse(request.responseText).data.timings );
+  } else {
+    // We reached our target server, but it returned an error
+
+  }
+};
+
+request.onerror = function() {
+  // There was a connection error of some sort
+};
+
+request.send();
+
+function renderTime(timings) {
+  console.log(timings);
+  for(var x in timings){
+    if(timings.hasOwnProperty(x)){
+      var a = timings[x].split(':');
+      var minutes = (+a[0]) * 60 + (+a[1]);
+      // hardcoded
+      var limaWaktu = ['fajr', 'dhuhr', 'asr', 'maghrib', 'isha'];
+      if (limaWaktu.indexOf(x.toLowerCase()) > -1 ){
+        var indicator = document.getElementsByClassName(x.toLowerCase())[0];
+        indicator.style.transform = 'rotate(' + minutes*0.25 + 'deg)';
+      }
+
+      // 0.25*minutes to set prayer time
+    }
   }
 }
